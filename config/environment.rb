f8072e236bab0f7f145510ce0c661340af496dd6
Maarten Brouwers <github.com@murb.nl>
2011-02-13 19:06:15 +0100

    gem problem?

--- config/environment.rb
+++ config/environment.rb
@@ -21,11 +21,11 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   
   config.gem 'nokogiri'
-  config.gem 'geokit'
   config.gem 'ym4r'
   config.gem 'authlogic'
   config.gem 'formtastic'
   config.gem 'BlueCloth'
+  config.gem 'geokit'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
