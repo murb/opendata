575428aae71998a8f8bb73bbf1a5dcab00433262
Maarten Brouwers <github.com@murb.nl>
2011-02-13 19:09:31 +0100

    rdiscount instead of bluecloth

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   config.gem 'ym4r'
   config.gem 'authlogic'
   config.gem 'formtastic'
-  config.gem 'BlueCloth'
+  config.gem 'rdiscount'
   config.gem 'geokit'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
